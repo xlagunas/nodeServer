8b20fba8252c1c053e8a8861a1a5fd138bf62960
Xavier Lagunas Calpe <xlagunas@gmail.com>
2014-01-23 18:35:32 +0100

    Created Persistence module, toying with it

--- persistence.js
+++ persistence.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/v2b');
-
+//mongoose.connect('mongodb://localhost/v2b');
 var callSchema = new mongoose.Schema(
     {
         caller: String,
@@ -19,39 +18,88 @@ var userSchema = new mongoose.Schema(
         email: {type: String, required: true},
         status: {type: String, default: 'OFFLINE'},
         password: {type: String, select: false, required: true},
-        contacts: [{username: 'String', relStatus: 'String'}],
-        joinDate: {type: Date, default: Date.now()},
-        calls: [callSchema]
+        contacts: {type: [{username: {type: 'String'}, relStatus: {type:'String', default: 'PENDING'}}], select: false},
+        joinDate: {type: Date, default: Date.now(), select:false},
+        thumbnail: {type: String, default: 'profile.png'},
+        calls: {type: [callSchema], select: false}
     }
 );
+userSchema.statics.findByUsername = function(username, cb){
+    this.findOne({username: username}, cb);
+};
+userSchema.statics.addRelationship = function(proposer, proposed, cb){
+    this.findOne({username: proposer}).select('+contacts').exec(function(error, user){
+        var result = {};
+        if (error){
+            result.status = 'error'
+            result.data = "User not found";
+        }
+        else{
+            user.contacts.push({'username': proposed});
+            result.status = 'success';
+            user.update();
+            if (cb) cb(result);
+        }
+    })
+}
+userSchema.statics.login = function(username, password, cb){
+    this.findOne({username: username}).select('+password').exec(function(err, user){
+        var result = {};
+        if (err){
+            result.status = 'error'
+            result.data = "User not found";
+        }
+        else{
+            if (user.password === password){
+                result.status = 'success';
+                result.data = user;
+            }
+            else{
+                result.status = 'error';
+                result.data = "Password doesn't match";
+            }
+        }
+        if (cb)
+            cb(result);
+    });
+};
+
+userSchema.methods.getContacts = function(relStatus, cb){
+    var queryObject = {'username': this.username},
+        result = {};
+
+    if (relStatus != 'ALL' || relStatus ==null){
+        queryObject.contacts = {'relStatus':  relStatus};
+    }
+
+    console.log("queryObject"+ JSON.stringify(queryObject));
+
+//    this.model('Usuari').findOne(queryObject).select('+contacts').exec(function(err, data){
+    this.model('Usuari').findOne(queryObject,['contacts'],function(err, data){
+        console.log(data);
+        if (err){
+            result.status = 'error'
+            result.data = "Error executing query";
+        }
+        else{
+            result.status = 'success'
+            result.data = data.contacts;
+        }
+        if (cb)
+            cb(result);
+    });
+}
 
 var User = mongoose.model('Usuari', userSchema);
     User.on('error', function(error){
         console.log("hi ha un error");
         console.log(error);
-    })
+    });
+
 
 var Call = mongoose.model('Trucada', callSchema);
 
-var xavi = new User(
-    {
-        username: 'xlagunas',
-        name: 'Xavier',
-        firstSurname: 'Lagunas',
-        lastSurname: 'Calpe',
-        email: 'xlagunas@gmail.com',
-        password: '123456'
-   }
-);
-
-    xavi.save(saveCallback);
-
-function saveCallback(error){
-    if (error)
-        console.log(error);
-    else
-        console.log("successfully saved");
-}
-
 
 //var relSchema = new mongoose.Schema({contact: mongoose.Schema.ObjectId, proposer: mongoose.Schema.ObjectId, date: Date, status: String});
+exports.userModel = User;
+exports.callModel = Call;
